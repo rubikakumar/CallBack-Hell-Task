f5f2b09975c7286e093e0dac779f4f549a5c13b3
rubikakumar <153973333+rubikakumar@users.noreply.github.com>
2024-03-10 19:12:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 10;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 9;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 8;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 7;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 6;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 5;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 4;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 3;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 2;
+
+setTimeout(function() {
+    document.getElementById('countdown').innerText = 1;
+
+setTimeout(function() {
+        document.getElementById('countdown').innerHTML = "<span style='color: maroon;'>🎆Happy Independence Day🎆</span>";
+                                      }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
